67e21d2f2f7227a39a7330ec4cf314990d6573bf
David Arnold <dja+djaboxx@sonic.net>
2018-12-03 11:56:54 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,11 @@
 //--------------------------------------------------------------------
-// Variables
-
-
-
-//--------------------------------------------------------------------
 // Modules
 module "network_host" {
   source  = "app.terraform.io/AWSDemoDarnoldTFE/network-host/aws"
   version = "2.2.0"
 
   instance_type = "m4.large"
-  network_ws = "ConfigDesigner"
+  network_ws = "Network"
   organization = "AWSDemoDarnoldTFE"
-  private_instances = 2
-}
-
-module "network" {
-  source  = "app.terraform.io/AWSDemoDarnoldTFE/network/aws"
-  version = "3.1.0"
-
-  availability_zones = ["us-east-1a"]
-  key_name = "tfe-demos-darnold"
-  network_name = "ConfigDesigner"
-  region = "us-east-1"
+  private_instances = 1
 }
